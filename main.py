0114590161af95350b6850d99eba5ca5d274794c
EddieWongED <aolaeddie@gmail.com>
2020-12-26 14:13:07 +0000

    emoji to the bot test

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ from discord.ext import commands
 import os
 from keep_alive import keep_alive
 
-
+bot_message_id = ""
+emoji_1 = '\U00000031'
+emoji_2 = '\U00000032'
+emoji_3 = '\U00000033'
+emoji_check = '\U00002705'
 client = discord.Client()
 # reaction = discord.reaction()
+wolf_message = False
+
 
 async def createlist(message):
   role = []
+  global wolf_message
+  wolf_message = True
   await message.channel.send('Number of wolves: ')
-  await bot.react(message, "name:one")
+
 
 @client.event
 async def on_reaction_add(reaction, user):
@@ -23,15 +31,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  global wolf_message
+  if wolf_message:
+    await message.add_reaction(emoji_check)
+    # await message.add_reaction(emoji_2)
+    # await message.add_reaction(emoji_3)
+    wolf_message = False
+  
   if message.author == client.user:
     return
 
-  msg = message.content
+  print(message.author.bot)
 
   if message.content.startswith('$hello'):
+    await message.add_reaction(emoji_1)
+    await message.add_reaction(emoji_2)
+    await message.add_reaction(emoji_3)
     await message.channel.send('Hello!')
-    await message.react(':upside_down:')
-
+    
   if message.content.startswith('$start'):
     await createlist(message)
 
