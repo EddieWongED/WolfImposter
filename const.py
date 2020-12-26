7a5d2f71488e0d0e8ff8f39397f00c55595eed09
EddieWongED <aolaeddie@gmail.com>
2020-12-26 16:21:02 +0000

    players choice done

---
+++ const.py
@@ -0,0 +1,21 @@
+emoji_1 = "1️⃣"
+emoji_2 = "2️⃣"
+emoji_3 = "3️⃣"
+emoji_4 = "4️⃣"
+emoji_5 = "5️⃣"
+emoji_6 = "6️⃣"
+emoji_7 = "7️⃣"
+emoji_8 = "8️⃣"
+emoji_9 = "9️⃣"
+emoji_check = '✅'
+numbers = {1:emoji_1, 2:emoji_2, 3:emoji_3, 4:emoji_4, 5:emoji_5, 6:emoji_6, 7:emoji_7, 8:emoji_8, 9:emoji_9}
+
+settings_str = '''Settings:
+>>>Set the amount of players: use $players
+>>>Set the amount of wolves: use $wolf
+>>>Set the amount of witch: use $witch
+>>>Set the amount of Prophet: use $prophet
+'''
+
+Starting_str = '''Waiting for players to join...
+React on the tick to join'''
--- main.py
+++ main.py
@@ -1,56 +1,86 @@
 import discord
 from discord.ext import commands
+import const
+import time
 import os
 from keep_alive import keep_alive
 
+in_game = []
+current_command = ""
+players = 0
 bot_message_id = ""
-emoji_1 = '\U00000031'
-emoji_2 = '\U00000032'
-emoji_3 = '\U00000033'
-emoji_check = '\U00002705'
 client = discord.Client()
 # reaction = discord.reaction()
-wolf_message = False
+
+def main():
+  keep_alive()
+  client.run(os.getenv("TOKEN"))
+
+
+async def askForPlayer(message):
+  playermsg = await message.channel.send('>>> ```Number of players: ```')
+  global bot_message_id
+  bot_message_id = playermsg.id
+  for i in range(1, 10):
+    await playermsg.add_reaction(const.numbers[i])
 
 
 async def createlist(message):
   role = []
-  global wolf_message
-  wolf_message = True
-  await message.channel.send('Number of wolves: ')
-
 
 @client.event
-async def on_reaction_add(reaction, user):
-    await print("1")
+async def on_raw_reaction_add(payload):
+  global bot_message_id
+  global current_command
+  if bot_message_id == payload.message_id and not(payload.member.bot):
+    if current_command == "help":
+      pass
+    elif current_command == "settings":
+      pass
+    elif current_command == "start":
+      if payload.emoji.name == const.emoji_check:
+        in_game.append(payload.member.name)
+      pass
+    elif current_command == "players":
+      for number in const.numbers:
+        if const.numbers[number] == payload.emoji.name:
+          global players
+          players = number
+          print(f'Players:{players}')
+    
+  
 
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-  global wolf_message
-  if wolf_message:
-    await message.add_reaction(emoji_check)
-    # await message.add_reaction(emoji_2)
-    # await message.add_reaction(emoji_3)
-    wolf_message = False
-  
+  print(message.content)
+  global current_command
   if message.author == client.user:
     return
 
-  print(message.author.bot)
+  if message.content.startswith('$help'):
+    current_command = 'help'
+    await message.channel.send(const.settings_str)
 
-  if message.content.startswith('$hello'):
-    await message.add_reaction(emoji_1)
-    await message.add_reaction(emoji_2)
-    await message.add_reaction(emoji_3)
-    await message.channel.send('Hello!')
-    
+  if message.content.startswith('$settings') or message.content.startswith('$set'):
+    current_command = 'settings'
+    await message.channel.send(f'''The settings now:
+>>> ```Amount of players in game: {len(in_game)}```
+```Amount of players until start: {players}
+Players```''')
+  
+  if message.content.startswith('$players') or message.content.startswith('$p'):
+    current_command = 'players'
+    await askForPlayer(message)
+
   if message.content.startswith('$start'):
-    await createlist(message)
+    current_command = 'start'
+    start = await message.channel.send(const.Starting_str)
+    await start.add_reaction(const.emoji_check)
 
-keep_alive()
-client.run(os.getenv("TOKEN"))
+main()
