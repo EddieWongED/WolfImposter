5c04f9137de89a348dbe41149dbd408e6922c851
HarryNCY <harryng20021030@gmail.com>
2020-12-26 18:05:02 +0000

    save

--- const.py
+++ const.py
@@ -10,12 +10,26 @@ emoji_9 = "9️⃣"
 emoji_check = '✅'
 numbers = {1:emoji_1, 2:emoji_2, 3:emoji_3, 4:emoji_4, 5:emoji_5, 6:emoji_6, 7:emoji_7, 8:emoji_8, 9:emoji_9}
 
+commands = {
+"settings": ["settings", "setting", "set", "s"], 
+"players": ["players", "player", "play", "p"], 
+"wolves": ["wolves", "wolf"],
+"witch": ["witch"],
+"prophet": ["prophet","prop"], 
+"start": ["begin", "start"], 
+"help": ["guide", "help"], 
+"role": ["role"]}
+
 settings_str = '''Settings:
 >>>Set the amount of players: use $players
->>>Set the amount of wolves: use $wolf
+>>>Set the amount of wolves: use $wolves
 >>>Set the amount of witch: use $witch
 >>>Set the amount of Prophet: use $prophet
 '''
 
 Starting_str = '''Waiting for players to join...
 React on the tick to join'''
+
+tooManyRoleError = '''***Error:***
+>>> ```css
+As the number of roles is greater than the number of players, the game is stopped.```'''
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import discord
-from discord.ext import commands
 import const
-import time
 import os
+import random
 from keep_alive import keep_alive
 
 in_game = []
+role_dict = {}
 current_command = ""
 players = 0
-bot_message_id = ""
+wolves = 1
+witch = 0
+prophet = 0
+bot_message_id = {}
+prefix = "$"
+onTask = False
 client = discord.Client()
 # reaction = discord.reaction()
 
 def main():
+  for command in const.commands:
+    bot_message_id[command] = ""
   keep_alive()
   client.run(os.getenv("TOKEN"))
 
@@ -20,36 +27,114 @@ def main():
 async def askForPlayer(message):
   playermsg = await message.channel.send('>>> ```Number of players: ```')
   global bot_message_id
-  bot_message_id = playermsg.id
+  bot_message_id["players"] = playermsg.id
   for i in range(1, 10):
     await playermsg.add_reaction(const.numbers[i])
 
+async def askForWolf(message):
+  wolfmsg = await message.channel.send('>>> ```Number of wolves: ```')
+  global bot_message_id
+  bot_message_id["wolves"] = wolfmsg.id
+  for i in range(1, 4):
+    await wolfmsg.add_reaction(const.numbers[i])
 
-async def createlist(message):
+async def askForWitch(message):
+  witchmsg = await message.channel.send('>>> ```Number of witch: ```')
+  global bot_message_id
+  bot_message_id["witch"] = witchmsg.id
+  for i in range(1, 4):
+    await witchmsg.add_reaction(const.numbers[i])
+
+async def askForProphet(message):
+  prophetmsg = await message.channel.send('>>> ```Number of prophet: ```')
+  global bot_message_id
+  bot_message_id["prophet"] = prophetmsg.id
+  for i in range(1, 4):
+    await prophetmsg.add_reaction(const.numbers[i])
+
+def createlist():
+  global wolves
+  global witch
+  global prophet
   role = []
+  for _ in range(wolves):
+    role.append('wolf')
+  for _ in range(witch):
+    role.append('witch')
+  for _ in range(prophet):
+    role.append('prophet')
+  print(f'Role List: {role}')
+  return role
+
+async def startgame(message):
+  global role_dict
+  global in_game
+  role_dict = {}
+  role = createlist()
+  if len(role) > len(in_game):
+    await message.channel.send(const.tooManyRoleError)
+    return
+  temp_players = in_game[:]
+  temp_role = role[:]
+  random.shuffle(temp_players)
+  for i in range(len(in_game)):
+    try:
+      role_dict[temp_players.pop()] = temp_role.pop()
+    except:
+      role_dict[temp_players.pop()] = 'Trash'
 
 @client.event
 async def on_raw_reaction_add(payload):
   global bot_message_id
   global current_command
-  if bot_message_id == payload.message_id and not(payload.member.bot):
+  global onTask
+  print(f'Current CMD: {current_command}')
+  print(f'Sensing {payload.emoji.name},{payload.member.name}' )
+  matched = False
+  for command in bot_message_id:
+    if (bot_message_id[command] == payload.message_id):
+      matched = True
+      break
+  if matched and not(payload.member.bot):
+    matched = False
     if current_command == "help":
       pass
     elif current_command == "settings":
       pass
     elif current_command == "start":
-      if payload.emoji.name == const.emoji_check:
+      if payload.emoji.name == const.emoji_check and payload.member.name not in in_game:
         in_game.append(payload.member.name)
+        print(in_game)
       pass
     elif current_command == "players":
       for number in const.numbers:
         if const.numbers[number] == payload.emoji.name:
           global players
           players = number
+          onTask = False
           print(f'Players:{players}')
+    elif current_command == "wolves":
+      for number in const.numbers:
+        if const.numbers[number] == payload.emoji.name:
+          global wolves
+          wolves = number
+          onTask = False
+          print(f'Wolves:{wolves}')
+    elif current_command == "witch":
+      for number in const.numbers:
+        if const.numbers[number] == payload.emoji.name:
+          global witch
+          witch = number
+          onTask = False
+          print(f'Witch:{witch}')
+    elif current_command == "prophet":
+      for number in const.numbers:
+        if const.numbers[number] == payload.emoji.name:
+          global prophet
+          prophet = number
+          onTask = False
+          print(f'Prophet:{prophet}')
     
-  
-
 
 @client.event
 async def on_ready():
@@ -58,29 +143,56 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  print(message.content)
+  global bot_message_id
+  global role_dict
+  global onTask
+  print(bot_message_id)
+  # print(f'The whole message:\n{message}')
   global current_command
+  if (onTask):
+    return
+  first_text = message.content.split()[0]
+  print(type(message.content))
   if message.author == client.user:
     return
 
-  if message.content.startswith('$help'):
+  if first_text[1:] in const.commands["help"] and first_text[0] == prefix:
     current_command = 'help'
     await message.channel.send(const.settings_str)
 
-  if message.content.startswith('$settings') or message.content.startswith('$set'):
+  elif first_text[1:] in const.commands["settings"] and first_text[0] == prefix:
     current_command = 'settings'
-    await message.channel.send(f'''The settings now:
->>> ```Amount of players in game: {len(in_game)}```
-```Amount of players until start: {players}
-Players```''')
-  
-  if message.content.startswith('$players') or message.content.startswith('$p'):
+    settings = await message.channel.send(f'***The settings now:***\n>>> ```css\nNumber of players: {players}``` ```css\nNumber of wolves: {wolves}``` ```css\nNumber of witch: {witch}``` ```css\nNumber of prophet: {prophet}``` ```css\nPlayers in game: {in_game}```')
+    bot_message_id["settings"] = settings.id
+    
+  elif first_text[1:] in const.commands["players"] and first_text[0] == prefix:
+    onTask = True
     current_command = 'players'
     await askForPlayer(message)
 
-  if message.content.startswith('$start'):
+  elif first_text[1:] in const.commands["wolves"] and first_text[0] == prefix:
+    onTask = True
+    current_command = 'wolves'
+    await askForWolf(message)
+  
+  elif first_text[1:] in const.commands["witch"] and first_text[0] == prefix:
+    onTask = True
+    current_command = 'witch'
+    await askForWitch(message)
+
+  elif first_text[1:] in const.commands["prophet"] and first_text[0] == prefix:
+    onTask = True
+    current_command = 'prophet'
+    await askForProphet(message)
+
+  elif first_text[1:] in const.commands["start"] and first_text[0] == prefix:
     current_command = 'start'
     start = await message.channel.send(const.Starting_str)
+    bot_message_id["start"] = start.id
     await start.add_reaction(const.emoji_check)
+    await startgame(message)
+
+  elif first_text[1:] in const.commands["role"] and first_text[0] == prefix:
+    await message.channel.send(role_dict)
 
 main()
